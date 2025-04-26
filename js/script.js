99957497b09808e13ad065c2085baeedd834f5e5
alisaid1235 <hahaali139@gmail.com>
2025-04-26 17:39:07 +0200

    slider in commetres of our clients

--- js/script.js
+++ js/script.js
@@ -1,32 +1,15 @@
 /* global $, alert, console */
 
 $(function () {
-    
-    'use strict';
+  ("use strict");
 
-    var myHeader = $('.header');    
-    var mySlider = $(".slider")
-        
-        
+  var myHeader = $(".header");
+  var mySlider = $(".slider");
+
+  myHeader.height($(window).height());
+
+  $(window).resize(function () {
     myHeader.height($(window).height());
-
-    $(window).resize(function () {
-      myHeader.height($(window).height());
-      // Adjust Bxlider List Item Center
-      mySlider.each(function () {
-        $(this).css(
-          "paddingTop",
-          ($(window).height() - $(".slider li").height()) / 2
-        );
-      });
-    });
-
-    // Links Add Active Class
-    $('.links li a').click(function () {
-        $(this).parent().addClass('active').siblings().removeClass('active')
-    });
-
-
     // Adjust Bxlider List Item Center
     mySlider.each(function () {
       $(this).css(
@@ -34,22 +17,57 @@ $(function () {
         ($(window).height() - $(".slider li").height()) / 2
       );
     });
-
-
-    // Trigger the Bx Slider 
-    mySlider.bxSlider({
-      pager: false,
-    });
-  
-  
-  // Smoth Scroll To Div 
-  $('.links li a').click(function () { 
-    console.log('clicked');
-    
-    $('html,body').animate({
-
-      scrollTop: $('#' + $(this).data('value')).offset().top
-      
-    },1000)
   });
+
+  // Links Add Active Class
+  $(".links li a").click(function () {
+    $(this).parent().addClass("active").siblings().removeClass("active");
+  });
+
+  // Adjust Bxlider List Item Center
+  mySlider.each(function () {
+    $(this).css(
+      "paddingTop",
+      ($(window).height() - $(".slider li").height()) / 2
+    );
+  });
+
+  // Trigger the Bx Slider
+  mySlider.bxSlider({
+    pager: false,
+  });
+
+  // Smoth Scroll To Div
+  $(".links li a").click(function () {
+    console.log("clicked");
+
+    $("html,body").animate(
+      {
+        scrollTop: $("#" + $(this).data("value")).offset().top,
+      },
+      1000
+    );
+  });
+
+  // Our Auto  Slider Code
+  (function autoSlider() {
+    $(".Slider .active").each(function () {
+      if (!$(this).is(":last-child")) {
+        console.log("yes");
+        $(this)
+          .delay(3000)
+          .fadeOut(1000, function () {
+            $(this).removeClass("active").next().addClass("active").fadeIn();
+            autoSlider();
+          });
+      } else {
+        $(this).delay(3000).fadeOut(1000, function () {
+          $(this).removeClass('active');
+          console.log('hah')
+          $('.Slider div').eq(0).addClass('active').fadeIn();
+          autoSlider();
+        });
+      }
+    });
+  }());
 });
